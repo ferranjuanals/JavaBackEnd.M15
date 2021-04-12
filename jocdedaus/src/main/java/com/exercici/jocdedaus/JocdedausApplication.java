3889fff7aafde8881b4c4ee972db1553819d0790
Ferran Juanals <ferranjuanals@gmail.com>
2021-04-12 15:48:42 +0200

    Spring project

---
+++ jocdedaus/src/main/java/com/exercici/jocdedaus/JocdedausApplication.java
@@ -0,0 +1,13 @@
+package com.exercici.jocdedaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JocdedausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JocdedausApplication.class, args);
+	}
+
+}
---
+++ jocdedaus/src/test/java/com/exercici/jocdedaus/JocdedausApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercici.jocdedaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JocdedausApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
