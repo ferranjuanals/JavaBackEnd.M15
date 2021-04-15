358801fd40ec66a41ec30688e7de04481eb4e7ed
Ferran Juanals <ferranjuanals@gmail.com>
2021-04-15 14:22:18 +0200

    Properties

--- jocdedaus/src/main/java/com/exercici/jocdedaus/JocdedausApplication.java
+++ jocdedaus/src/main/java/com/exercici/jocdedaus/JocdedausApplication.java
@@ -2,7 +2,11 @@ package com.exercici.jocdedaus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories(basePackages = {"com.exercici.jocdedaus.repositories.mongodb"})
+@EnableJpaRepositories(basePackages = {"com.exercici.jocdedaus.repositories.mysql"})
 @SpringBootApplication
 public class JocdedausApplication {
 
